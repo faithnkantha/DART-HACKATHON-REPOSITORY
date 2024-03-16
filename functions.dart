9fb74708f01fe27a4fd6c27acba36af85b9d2b89
faith nkatha <faithnkatha160@gmail.com>
2024-03-17 00:05:59 +0300

    [hackathon]

--- functions.dart
+++ functions.dart
@@ -1,2 +1,18 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+//Program 2: Perform Mathematical Operations with Functions
+//Write a Dart program that performs two mathematical operations using functions.
+
+int add(num1,num2){
+ int sum = num1+num2;
+ return sum;
+
+}
+
+void mul(var1,var2){
+  int total = var1 *var2;
+  print(total);
+
+}
+void main(){
+  print(add(2,4));
+  mul(45, 2);
+}
